ecb0143129b51777a9debaa85e707b9206343a05
Bot1noke <alexsys006.ivanov@gmail.com>
2023-10-07 12:04:24 +0300

    first

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+import pandas as pd
+from typing import List
+
+def open_file(path: str) -> List[List[object]]:
+    df = pd.read_csv(path)
+
+    return df.values.tolist()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="our cute spotify experience")
+
+    parser.add_argument("file_path", type=str, help="input path to the table")
+
+    args = parser.parse_args()
+
+    table = open_file(args.file_path)
+
+    print(len(table))
